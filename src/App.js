892f49470efa86346fcf06a4030e48d32a6a0a70
Basu-Dev-Ghosh <basu1735@gmail.com>
2023-01-03 20:57:57 +0530

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
-import Page05 from "./Pages/Page_05";
-import Page06 from "./Pages/Page_06";
-import Page07 from "./Pages/Page_07";
-import Page08 from "./Pages/Page_08";
-import Page09 from "./Pages/Page_09";
-import Page10 from "./Pages/Page_10";
-
 import { Routes, Route } from "react-router-dom";
-import PrivateRoutes from "./PrivateRoutes";
 import Signup from "./Pages/Signup/Signup";
 import Signup2 from "./Pages/Signup2/Signup2";
 import SearchResult from "./Pages/SearchResult/SearchResult";
@@ -29,12 +21,6 @@ function App() {
         <Route path="/signup2" element={<Signup2 />} />
         <Route path="/search-result/:searchinput" element={<SearchResult />} />
         <Route path="/company/:name/:id" element={<Company />} />
-        <Route path="/page05/:id" exact element={<PrivateRoutes Component={Page05} />} />
-        <Route path="/page06/:id" element={<Page06 />} />
-        <Route path="/page07/:id" element={<Page07 />} />
-        <Route path="/page09" exact element={<PrivateRoutes Component={Page09} />} />
-        <Route path="/page08/:name/:location" element={<Page08 />} />
-        <Route path="/page10" element={<Page10 />} />
       </Routes>
 
     </>
